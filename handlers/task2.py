64665dc6d20e2cbde8d1940ee7ff60c6e8ca8364
Evgeniy Grakovich <grakovich-e@yandex.by>
2023-09-15 15:04:08 +0300

    more answers

--- handlers/task2.py
+++ handlers/task2.py
@@ -15,6 +15,9 @@ async def send_welcome(message: types.Message, state: FSMContext):
     elif message.text == texts.answer_btn:
         await message.answer(texts.enter_answer, reply_markup=kb.get_hint_kb)
         await State.task_2_answering.set()
+    elif logic.remove_punctuation(message.text.upper()) in texts.task2_wrong_ans:
+        ans = texts.task2_wrong_ans.get(logic.remove_punctuation(message.text.upper()))
+        await message.answer(ans, reply_markup=kb.answer_or_hint_kb)
     else:
         await message.answer(texts.use_a_button, reply_markup=kb.answer_or_hint_kb)
 
@@ -26,7 +29,8 @@ async def send_welcome(message: types.Message, state: FSMContext):
         await message.answer(texts.task2_hint_1, reply_markup=kb.answer_or_hint_kb)
         await State.task_2_3.set()
     elif logic.remove_punctuation(answer.upper()) in texts.task2_wrong_ans:
-        await message.answer(texts.no_need_question_2, reply_markup=kb.get_hint_kb)
+        ans = texts.task2_wrong_ans.get(logic.remove_punctuation(message.text.upper()))
+        await message.answer(ans, reply_markup=kb.get_hint_kb)
     elif answer.upper() == texts.task2_3_ans:
         await message.answer(texts.task_2_finish, reply_markup=kb.continue_kb)
         await State.asking_for_continue.set()
